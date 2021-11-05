064b4b973944a02584a52696c57d177b7924106b
chadohl <chadohl@corporatetools.com>
2021-11-05 16:32:04 -0700

    Added vue-playlist project from lesson 17 & 18

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Ninja App'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs-playlist/src/Ninja.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Ninja.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
